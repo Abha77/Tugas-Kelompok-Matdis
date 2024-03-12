5f4b201d0498e1735281d219a6a708c83ba3a9df
Abha77 <145103444+Abha77@users.noreply.github.com>
2024-03-12 22:33:19 +0700

    Update Sistem Pembayaran Digital.cpp
    menambah fitur enkripsi dan dekripsi

--- Sistem Pembayaran Digital.cpp
+++ Sistem Pembayaran Digital.cpp
@@ -16,6 +16,81 @@ const int MAKS = 10;
 int n;
 data_akun user[MAKS];
 
+void tampilan1(int *p);
+void update_top_up(int *saldo, int nominal);
+void transfer(int i, string NIM, int nominal);
+int read_data();
+void write_data();
+void menu(int i);
+void login();
+void sign_up();
+
+string enkripsi(string plaintext){
+    int panjang_pesan = plaintext.length();
+    string chiphertext = "";
+    int shift = 13;
+    char text;
+    for(int i = 0; i < panjang_pesan; i++){
+        text = plaintext[i];
+        if(isalpha(text)){
+            if(isupper(text)){
+                text = (text - 'A' + shift) % 26 + 'A';
+                chiphertext += text;
+            }else{
+                text = (text - 'a' + shift) % 26 + 'a';
+                chiphertext += text;
+            }
+        }else if(isdigit(text)){
+            text = (text - '0' + shift) %10 + '0';
+            chiphertext += text;
+        }else if(text == ' '){
+            chiphertext += text;
+        }
+    }
+    return chiphertext;
+}
+
+string dekripsi(string ciphertext){
+    int panjang_pesan = ciphertext.length();
+    string plaintext = "";
+    int shift = 13;
+    char text;
+    for(int i = 0; i < panjang_pesan; i++){
+        text = ciphertext[i];
+        if(isalpha(text)){
+            if(isupper(text)){
+                text = (text - 'A' + 26 - shift) % 26 + 'A';
+                plaintext += text;
+            }else{
+                text = (text - 'a' + 26 - shift) % 26 + 'a';
+                plaintext += text;
+            }
+        }else if(isdigit(text)){
+            text = (((text - '0') - shift) + 10) % 10 + '0';
+            plaintext += text;
+        }else if(text == ' '){
+            plaintext += text;
+        }
+    }
+    return plaintext;
+}
+
+int main(){
+    lanjut:
+    system("cls");
+    int pilihan = 0;
+    tampilan1(&pilihan);
+    switch(pilihan){
+        case 1:
+            login();
+            goto lanjut;
+        case 2:
+            sign_up();
+            goto lanjut;
+    }
+
+}
+
 void tampilan1(int *p){
     system("cls");
     int pilihan;
@@ -61,23 +136,17 @@ int read_data(){
     while (!baca.eof()){
         getline(baca, NIM);
         getline(baca, nama);
-        getline(baca, saldo); int int_saldo = atoi(saldo.c_str());
+        getline(baca, saldo); saldo = dekripsi(saldo); int int_saldo = atoi(saldo.c_str());
         getline(baca, PIN);
 
-        user[i].NIM = NIM;
-        user[i].nama = nama;
+        user[i].NIM = dekripsi(NIM);
+        user[i].nama = dekripsi(nama);
         user[i].saldo = int_saldo;
-        user[i].PIN = PIN;
+        user[i].PIN = dekripsi(PIN);
 
         if(NIM.empty()){
             break;
         }
-
-       cout << user[i].NIM << endl;
-       cout << user[i].nama << endl;
-       cout << user[i].saldo << endl;
-       cout << user[i].PIN << endl;
-        
         i++;
     }
     baca.close();
@@ -91,10 +160,10 @@ void write_data(){
 
     int i = 0;
     while(user[i].NIM != "\0"){
-        tulis << user[i].NIM << endl;
-        tulis << user[i].nama << endl;
-        tulis << user[i].saldo << endl;
-        tulis << user[i].PIN << endl;
+        tulis << enkripsi(user[i].NIM) << endl;
+        tulis << enkripsi(user[i].nama) << endl;
+        tulis << enkripsi(to_string(user[i].saldo)) << endl;
+        tulis << enkripsi(user[i].PIN) << endl;
         i++;
     }
     tulis.close();
@@ -191,20 +260,4 @@ void sign_up(){
     user[n].saldo = 0;
     write_data();
     cin.get();
-}
-
-int main(){
-    lanjut:
-    system("cls");
-    int pilihan = 0;
-    tampilan1(&pilihan);
-    switch(pilihan){
-        case 1:
-            login();
-            goto lanjut;
-        case 2:
-            sign_up();
-            goto lanjut;
-    }
-
 }
